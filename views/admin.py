27f19b887ea541bc352730d56849221897d7b76e
HazemMeqdad <hazemmeqdad@gmail.com>
2022-03-22 13:35:06 +0300

    add a admin panel

---
+++ views/admin.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template, request, redirect, url_for, abort
+
+
+admin = Blueprint('admin', __name__, url_prefix='/admin')
+
+@admin.route('/')
+def index():
+    return render_template('admin.html')
+
