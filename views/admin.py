a2c04aee16147782b914a7ae195dd900b4135fcc
HazemMeqdad <66125469+HazemMeqdad@users.noreply.github.com>
2022-04-08 10:43:09 +0300

    Fix admin pages all working

--- views/admin.py
+++ views/admin.py
@@ -19,12 +19,14 @@ def logs():
     user = convert_data_user(app.logins.find_one({"token.access_token": session["token"]}))
     return render_template('logs.html', user=user, title="سجلات التسجيل بالموقع")
 
-@index.route('/azkar')
+@admin.route('/azkar')
 @only_admin
 def azkar_page():
-    return render_template('azkar.html', title="الاذكار")
+    user = convert_data_user(app.logins.find_one({"token.access_token": session["token"]}))
+    return render_template('azkar.html', user=user, title="الاذكار")
 
-@index.route('/suggestions')
+@admin.route('/suggestions')
 @only_admin
 def log_sug_page():
-    return render_template('log-sug.html', title="سجلات الاقتراحات")
+    user = convert_data_user(app.logins.find_one({"token.access_token": session["token"]}))
+    return render_template('log-sug.html', user=user, title="سجلات الاقتراحات")
