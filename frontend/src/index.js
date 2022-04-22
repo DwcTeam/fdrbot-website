569e7e29476554c56e794571a93107bb359ad195
HazemMeqdad <66125469+HazemMeqdad@users.noreply.github.com>
2022-04-22 15:20:52 +0300

    Remove some erros from console web

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import AppWrapper from "./App";
 
-
-ReactDOM.render(
-    <AppWrapper />,
-    document.getElementById("root")
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<AppWrapper />);
