6c22a4eaa69d497b1b87d71fb0635ab3236b2a86
HazemMeqdad <hazemmeqdad@gmail.com>
2022-05-27 11:07:54 +0300

    Fix outh url endpoint

--- frontend/src/Pages/Dashboard/Outh.jsx
+++ frontend/src/Pages/Dashboard/Outh.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import  { Navigate } from 'react-router-dom'
 import * as queryString from "query-string";
 import axios from "axios";
-
+import { api_url } from "../../Config";
 
 class Outh extends React.Component {
     render(){
@@ -18,7 +18,8 @@ class Outh extends React.Component {
                 <Navigate to="/login" replace={true} />
             )
         }
-        axios.post("/auth/login", {code: params.code}).then(res => {
+        
+        axios.post(api_url + "/auth/login", {code: params.code}).then(res => {
             localStorage.setItem("token", res.data.token);
             console.log(res.data)
             setTimeout(() => {
